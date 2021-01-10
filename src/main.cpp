6f3890b68b5c425e18a6b19fd98202ab518ea36e
unknown <maximilian.gerhardt@rub.de>
2021-01-10 12:35:34 +0100

    Make it compilable again

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,19 @@
 #pragma GCC system_header
 #endif
 #endif
+/* these includes cause no error. */
+#include "ArduinoJson/Array/ArrayRef.hpp"
+#include "ArduinoJson/Object/ObjectRef.hpp"
+#include "ArduinoJson/Variant/VariantRef.hpp"
+#include "ArduinoJson/Document/DynamicJsonDocument.hpp"
+#include "ArduinoJson/Document/StaticJsonDocument.hpp"
+#include "ArduinoJson/Array/ArrayImpl.hpp"
+#include "ArduinoJson/Array/ElementProxy.hpp"
+#include "ArduinoJson/Array/Utilities.hpp"
+#include "ArduinoJson/Collection/CollectionImpl.hpp"
+#include "ArduinoJson/Object/MemberProxy.hpp"
+#include "ArduinoJson/Object/ObjectImpl.hpp"
+#include "ArduinoJson/Variant/VariantAsImpl.hpp"
 
 //commenting or uncommenting this 
 //makes a "function not used" defect show up
