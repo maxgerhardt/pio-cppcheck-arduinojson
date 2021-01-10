a00830d5e8133b36a6095fb0b292391e6e112a24
unknown <maximilian.gerhardt@rub.de>
2021-01-10 12:34:24 +0100

    Further narrow down error

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include "display.h"
+
+//originally "#includ <ArduinoJson.h>" would cause the error.
+//further narrowing it down brought it to one .h file that causes it
+#ifdef __cplusplus
+
+/* #include "ArduinoJson.hpp" manually 'inlined' */ 
+#include "ArduinoJson/Configuration.hpp"
+#if !ARDUINOJSON_DEBUG
+#ifdef __clang__
+#pragma clang system_header
+#elif defined __GNUC__
+#pragma GCC system_header
+#endif
+#endif
+
 //commenting or uncommenting this 
 //makes a "function not used" defect show up
 //pio check --skip-packages -v
-#include <ArduinoJSON.h>
+#include "ArduinoJson/Variant/VariantCompare.hpp"
+#endif
 
 // cppcheck-suppress unusedFunction
 void setup() {
